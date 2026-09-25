44beb037daa2eb4d2c18e6a80dc388ffcef28877
Vaishnavi Boje <56465126+v850@users.noreply.github.com>
2026-09-25 22:45:00 +0530

    Solve best-day-for-ad-revenue

--- practice/sql/best-day-for-ad-revenue/solution.sql
+++ practice/sql/best-day-for-ad-revenue/solution.sql
@@ -3,5 +3,8 @@ select *, clicked*revenue as rev, extract(DAY from impression_time) as day_of_mo
 from ad_impressions order by day_of_month asc 
 )
 -- select max(revenue) from cte group by cast(day_of_month as string) order by cast(day_of_month as string) asc;
-select day_of_month, avg(rev) as	avg_revenue, max(revenue) as	max_revenue, Null as	click_premium 
+select day_of_month, avg(rev) as	avg_revenue, max(revenue) as	max_revenue, AVG(CASE WHEN clicked = 1 THEN revenue END)
+-
+AVG(CASE WHEN clicked = 0 THEN revenue END)
+as	click_premium 
 from cte group by cast(day_of_month as INT) order by cast(day_of_month as INT) asc;

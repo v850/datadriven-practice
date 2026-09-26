f19814cbb2173913582fd17ba0c47088a0634664
Vaishnavi Boje <56465126+v850@users.noreply.github.com>
2026-09-26 16:42:37 +0530

    Solve the-regulars

--- practice/sql/the-regulars/solution.sql
+++ practice/sql/the-regulars/solution.sql
@@ -2,8 +2,7 @@ WITH cte AS(
 select distinct user_id ,
  extract(year FROM session_start) * 100 + extract(month FROM session_start) AS year_mon
   FROM user_sessions )
---  row_number() over(partition by  (extract(year from session_start)*100+ extract(month from session_start)) 
--- ,user_id ) as rnk
+
 
  
   select distinct user_id from (

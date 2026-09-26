14cd2cfe4c3399d564ae7a10ad1507ddccccb000
Vaishnavi Boje <56465126+v850@users.noreply.github.com>
2026-09-26 16:12:50 +0530

    Solve all-known-endpoints

--- practice/sql/all-known-endpoints/solution.sql
+++ practice/sql/all-known-endpoints/solution.sql
@@ -1,24 +1,20 @@
 WITH cte AS(SELECT DISTINCT endpoint FROM api_calls UNION SELECT DISTINCT b.endpoint FROM rate_limits b)
 
 
--- ,cte3 as (
--- SELECT 
---    CASE WHEN SUBSTR(endpoint,  1,  12) = '/api/v1/auth' THEN '/api/v1/auth' 
--- WHEN SUBSTR(endpoint,1,12) = '/api/v2/auth' THEN '/api/v2/auth' else endpoint end as endpoint,
---  Case
--- WHEN SUBSTR(endpoint,1,8) = '/api/v1/' THEN concat('v1',replace(endpoint,'/api/v1/',''))
---  WHEN SUBSTR(endpoint,1,8) = '/api/v2/' THEN concat('v2',replace(endpoint,'/api/v2/','')) 
---  end AS api1
--- FROM cte )
 ,cte3 as (
 SELECT 
    CASE WHEN SUBSTR(endpoint,  1,  12) = '/api/v1/auth' THEN '/api/v1/auth' 
-WHEN SUBSTR(endpoint,1,12) = '/api/v2/auth' THEN '/api/v2/auth' else endpoint end as endpoint,
+WHEN SUBSTR(endpoint,1,12) = '/api/v2/auth' THEN '/api/v2/auth' 
+when substr(endpoint,len(endpoint))='/' then substr(endpoint,1,len(endpoint)-1)
+else endpoint end as endpoint,
  Case
 WHEN SUBSTR(endpoint,1,8) = '/api/v1/' THEN replace(endpoint,'/','')
  WHEN SUBSTR(endpoint,1,8) = '/api/v2/' THEN replace(endpoint,'/','')
  end AS api1
 FROM cte )
 
-select distinct case when substr(endpoint,len(endpoint))='/' then substr(endpoint,1,len(endpoint)-1)
-else endpoint end as endpoint  from cte3 order by api1 asc;
+select distinct 
+-- case when substr(endpoint,len(endpoint))='/' then substr(endpoint,1,len(endpoint)-1)
+-- else 
+  -- endpoint end as 
+  endpoint  from cte3 order by api1 asc;

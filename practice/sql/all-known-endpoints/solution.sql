2de1e62b8b848df8dc58cfbb2ee046c1ed2d8281
Vaishnavi Boje <56465126+v850@users.noreply.github.com>
2026-09-26 16:07:24 +0530

    Solve all-known-endpoints

--- practice/sql/all-known-endpoints/solution.sql
+++ practice/sql/all-known-endpoints/solution.sql
@@ -6,8 +6,8 @@ SELECT
    CASE WHEN SUBSTR(endpoint,  1,  12) = '/api/v1/auth' THEN '/api/v1/auth' 
 WHEN SUBSTR(endpoint,1,12) = '/api/v2/auth' THEN '/api/v2/auth' else endpoint end as endpoint,
  Case when
-SUBSTR(endpoint, 1,  12) = '/api/v1/auth' THEN '/api/v1/auth' 
-WHEN SUBSTR(endpoint,1,12) = '/api/v2/auth' THEN '/api/v2/auth'
+SUBSTR(endpoint, 1,  12) = '/api/v1/auth' THEN 'apiv1auth' 
+WHEN SUBSTR(endpoint,1,12) = '/api/v2/auth' THEN 'apiv2auth'
 WHEN SUBSTR(endpoint,1,8) = '/api/v1/' THEN concat('v1',replace(endpoint,'/api/v1/',''))
  WHEN SUBSTR(endpoint,1,8) = '/api/v2/' THEN concat('v2',replace(endpoint,'/api/v2/','')) 
  end AS api1
